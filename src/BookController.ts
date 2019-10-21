6b85df3d77c98d90d00ba67ce2c8c3bfa148a0a8
SaitejDandge <saitejdandge1@gmail.com>
2019-10-22 02:55:32 +0530

    Optimizing code

--- src/BookController.ts
+++ src/BookController.ts
@@ -6,8 +6,21 @@ class BookController extends BaseController<Book> {
 
     constructor(endPoint, collectionName) {
         super(endPoint, collectionName, Book);
+        this.updateRouters();
 
     }
+
+    updateRouters() {
+        this.router.get(this.endPoint + "/test", (req, res) => {
+            console.log("hey")
+        });
+
+        this.router.post(this.endPoint+"/deete",(req,res)=>{
+
+            this.getModel().getModelSchema().update({});
+        });
+    }
+
 
 }
 
